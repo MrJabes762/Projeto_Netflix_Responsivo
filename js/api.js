bbb586d0f43e2bbc9e1ae880069158248cc44120
Mr Jabes <jabessilva31@gmail.com>
2024-08-28 23:45:31 -0300

    Refazendo Projeto

--- js/api.js
+++
@@ -1,4 +0,0 @@
-const api_key = "3716e8568d848ff93cecae386f39065f";
-const img_url = "https://image.tmdb.org/t/p/w500/";
-const genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
-const movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
